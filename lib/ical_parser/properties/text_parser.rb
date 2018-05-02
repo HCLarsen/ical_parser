4a5c01ba8986da4ee15562a4a5fdc1406ec2e0b6
Chris Larsen <clarsenipod@gmail.com>
2018-05-02 14:08:00 -0400

    Add Text Parsing

--- lib/ical_parser/properties/text_parser.rb
+++ lib/ical_parser/properties/text_parser.rb
@@ -1,11 +1,26 @@
 module IcalParser
   class TextParser
-    def self.parse(string)
+    def self.unescape(string)
       string.gsub(/(\\(?!\\))/){ |match| "" }
     end
 
-    def self.generate(string)
+    def self.escape(string)
       string.gsub(/(\,|\;|\\[^n])/){ |match| "\\" + match }
+    end
+
+    def self.parse(propName, eventc, optional = true)
+      regex = /#{propName.upcase}:(.*?)\R(?=\w)/m
+
+      matches = eventc.scan(regex)
+      if matches.count == 1
+        unescape(matches.first.first.strip)
+      elsif matches.size > 1
+        raise "Invalid Event: #{propName} MUST NOT occur more than once"
+      elsif !optional
+        raise "Invalid Event: #{propName} is REQUIRED"
+      else
+        nil
+      end
     end
   end
 end
--- test/properties/text_parser_test.rb
+++ test/properties/text_parser_test.rb
@@ -4,15 +4,48 @@ class TextParserTest < Minitest::Test
 
   include IcalParser
 
-  def test_parses_escaped_text
-    escaped = "I need to escape \\, \\; \\\\ but not \n newline characters"
-    text = "I need to escape , ; \\ but not \n newline characters"
-    assert_equal text, TextParser.parse(escaped)
+  def setup
+    @eventc = <<~HEREDOC
+    BEGIN:VEVENT
+    SUMMARY:Lunchtime meeting
+    UID:ff808181-1fd7389e-011f-d7389ef9-00000003
+    DTSTART;TZID=America/New_York:20160420T120000
+    DURATION:PT1H
+    DESCRIPTION: We'll continue with the unfinished business from last time,
+     in particular:
+       Can names dtstart with a number?
+       What if they are all numeric?
+       Reuse of names - is it valid
+     I remind the attendees we have spent 3 months on these subjects. We need
+     closure!!!
+    LOCATION:Mo's bar - back room
+    END:VEVENT
+    HEREDOC
   end
 
-  def test_generates_escaped_text
+  def test_unescapes_text
+    escaped = "I need to escape \\, \\; \\\\ but not \n newline characters"
+    text = "I need to escape , ; \\ but not \n newline characters"
+    assert_equal text, TextParser.unescape(escaped)
+  end
+
+  def test_escapes_text
     text = "I need to escape , ; \\ but not \n newline characters"
     escaped = "I need to escape \\, \\; \\\\ but not \n newline characters"
-    assert_equal escaped, TextParser.generate(text)
+    assert_equal escaped, TextParser.escape(text)
+  end
+
+  def test_parses_text
+    description_string = <<~DESCRIPTION_STRING
+    We'll continue with the unfinished business from last time,
+     in particular:
+       Can names dtstart with a number?
+       What if they are all numeric?
+       Reuse of names - is it valid
+     I remind the attendees we have spent 3 months on these subjects. We need
+     closure!!!
+    DESCRIPTION_STRING
+
+    assert_equal description_string.strip, TextParser.parse("description", @eventc)
   end
 end
