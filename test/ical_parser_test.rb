4744254fcd71e591f261dec3061dfe0a759ddc9e
Chris Larsen <clarsenipod@gmail.com>
2018-05-11 10:55:25 -0400

    Remove default test

--- test/ical_parser_test.rb
+++ test/ical_parser_test.rb
@@ -4,8 +4,4 @@ class IcalParserTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::IcalParser::VERSION
   end
-
-  def test_it_does_something_useful
-    assert true
-  end
 end
