aed5ef0acc7e9304c3c6c9b4983f32fceea7ded1
Chris Larsen <clarsenipod@gmail.com>
2018-05-12 10:33:43 -0400

    Clean up CalAddress Parser

--- lib/ical_parser/properties/cal_address_parser.rb
+++ lib/ical_parser/properties/cal_address_parser.rb
@@ -2,17 +2,8 @@ require 'uri'
 
 module IcalParser
   module CalAddressParser
-    CN_REGEX = /(?<name>CN=[\w\s]*):(?<uri>MAILTO:[\w\.@]*)/
-    FACEBOOK_REGEX = /(?<name>CN=[\w\s]*);(?<status>[\w=]*):(?<uri>https:\/{2}[\w\.@\/]*)/
-
     def self.parse(string)
-      if match = CN_REGEX.match(string)
-        uri = match["uri"]
-      elsif match = FACEBOOK_REGEX.match(string)
-        uri = match["uri"]
-      else
-        uri  = string
-      end
+      /(?<uri>(MAILTO|mailto|https):\/{0,2}[\w\.@\/]*)/ =~ string
       URI.parse(uri)
     end
   end
