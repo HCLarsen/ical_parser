6714553a4bbbc821a2074965ff0034575355507c
Chris Larsen <clarsenipod@gmail.com>
2018-07-04 15:13:28 -0400

    Convert Date-Time Regex patterns to constants

--- lib/ical_parser/properties/date_time_parser.rb
+++ lib/ical_parser/properties/date_time_parser.rb
@@ -3,16 +3,17 @@ require 'time'
 
 module IcalParser
   module DateTimeParser
-    def self.parse(string)
-      dTUTCRegex = /^\d{8}T\d{6}Z$/
-      dTRegex = /^\d{8}T\d{6}(?!Z)$/
-      dTTZRegex = /TZID=\w*\/\w*:\d{8}T\d{6}$/
+    DT_UTC_REGEX      = /^\d{8}T\d{6}Z$/
+    DT_FLOATING_REGEX = /^\d{8}T\d{6}$/
+    DT_TIME_ZONE_REGEX = /TZID=\w*\/\w*:\d{8}T\d{6}$/
 
-      if dTUTCRegex.match(string)
+    def self.parse(string)
+
+      if DT_UTC_REGEX.match(string)
         Time.parse(string)
-      elsif dTRegex.match(string)
+      elsif DT_FLOATING_REGEX.match(string)
         Time.parse(string)
-      elsif dTTZRegex.match(string)
+      elsif DT_TIME_ZONE_REGEX.match(string)
         self.parse_date_time_with_zone(string)
       else
         raise "Invalid Date-Time format"
